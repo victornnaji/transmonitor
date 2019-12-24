5856e61506d81b161b97f0af4ecce6aa751c35ba
Nnaji Victor <nnajivictor0@gmail.com>
2019-12-24 13:11:20 +0100

    implemented routing for the app

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,1 +1,15 @@
-racf
+import React from 'react';
+import App from "../App";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
